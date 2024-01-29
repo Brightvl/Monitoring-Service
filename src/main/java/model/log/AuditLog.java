6e116d1d52d7c2c073151657a72d10e1d479074a
Iaroslav S <ziro-x-100@mail.ru>
2024-01-29 13:42:05 +0300

    Done 1

--- src/main/java/model/log/AuditLog.java
+++
@@ -1,7 +0,0 @@
-package model.log;
-
-public class AuditLog {
-    public static void log(String action, String username) {
-        System.out.println("Действие: " + action + " пользователь: " + username);
-    }
-}
